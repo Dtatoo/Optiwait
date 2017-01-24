b0427995fe3804afa94a71e98e5ed53e54348bf0
Kevin Kim <kevinkim@KevinMacBookPro.local>
2017-01-23 20:50:16 -0600

    fixed up webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -1,27 +1,26 @@
-var path = require("path");
-var webpack = require("webpack");
-var ExtractTextPlugin = require("extract-text-webpack-plugin");
+const path = require("path");
+const webpack = require("webpack");
+const ExtractTextPlugin = require("extract-text-webpack-plugin");
 
-
-var elmSource = __dirname + "/web/elm";
 
 function join(dest) {
   return path.resolve(__dirname, dest);
 }
 
+const elmSource = __dirname + "/web/elm";
+const cssMain = join('web/static/css/app.css');
+const elmMain = join('web/elm/Main.elm');
+const javascriptMain = join('web/static/js/app.js');
+
 const config = module.exports = {
   entry: [
-    './web/static/css/app.css',
-    "./web/static/js/app.js",
-    "./web/elm/Main.elm",
+    cssMain,
+    elmMain,
+    javascriptMain,
   ],
   output: {
-    path: join("priv/static"),
+    path: join('priv/static'),
     filename: "js/app.js",
-  },
-  resolve: {
-    modules: [ "node_modules", __dirname + "/web/static/js" ],
-    extensions: [".js", ".elm"],
   },
   module: {
     noParse: [/\.elm/],
@@ -40,7 +39,7 @@ const config = module.exports = {
         test: /\.css/,
         exclude: /node_modules/,
         loader: ExtractTextPlugin.extract({
-          fallbackLoader: "style-loader",
+          fallbackLoader: 'style-loader',
           loader: [
             'css-loader?modules&importLoaders=1',
             'postcss-loader'
