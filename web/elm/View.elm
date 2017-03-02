08dc4e9a4d22283c5ea9a4d2474a871a005bcc46
Kevin Kim <jok068@mail.usask.ca>
2017-03-01 21:25:35 -0600

    added login page as a page element

--- web/elm/View.elm
+++ web/elm/View.elm
@@ -16,8 +16,14 @@ view : AppModel -> Html Msg
 view model =
     div []
         [ homePageView
+        , loginPage model
         , page model
         ]
+
+
+loginPage : AppModel -> Html Msg
+loginPage model =
+    Html.map LoginMsg (Login.view model.login)
 
 
 page : AppModel -> Html Msg
